073e2a0ad839b5ab790a1d53f5d8776837653cf8
vit <9455469@mail.ru>
2025-02-04 12:39:55 +0300

    32.2

--- gl/new/scene/generatorWF/myGeneratorWFZmyka.js
+++ gl/new/scene/generatorWF/myGeneratorWFZmyka.js
@@ -4,6 +4,7 @@ class MyGeneratorWFZmyka
 {
 	pointsObj = [];
 	linesObj = [];
+	typeZmyka = 1;
 
 	// создаем змейку
 	crZmyka({dataGrid})
@@ -118,16 +119,39 @@ class MyGeneratorWFZmyka
 			const arrP = []; 
 			const count = myMath.numberRound({value: dist/sizeCell, type: 'floor'});
 			
-			for ( let i = 0; i < count; i++ )
+			if(this.typeZmyka === 1)
 			{
-				const pos = myMath.multiVector3({dir, num: sizeCell * i});
-				pos.add(v[0]);
-				arrP.push(pos);
-				
-				//this.crHelpBox({pos});
+				for ( let i = 0; i < count; i++ )
+				{
+					const pos = myMath.multiVector3({dir, num: sizeCell * i});
+					pos.add(v[0]);
+					arrP.push(pos);
+					
+					//this.crHelpBox({pos});
+				}				
 			}
-			
-			
+			else
+			{
+				let stepDist = 0;
+				let i = 0;
+				
+				while (count * sizeCell > stepDist)
+				//for ( let i = 0; i < count; i++ )
+				{
+					
+					let kof = (i % 2 === 0) ? 3 : 1;
+					
+					const pos = myMath.multiVector3({dir, num: stepDist});
+					pos.add(v[0]);
+					arrP.push(pos);
+					console.log(kof, sizeCell * kof, stepDist)
+					stepDist += sizeCell * kof;
+					i++;
+					
+					this.crHelpBox({pos});
+				}				
+			}
+
 			
 			const normal = myMath.calcNormal2D({p1: v[0], p2: v[1], reverse: false})
 			
@@ -148,7 +172,6 @@ class MyGeneratorWFZmyka
 		if(1===1)
 		{
 			const arrL2 = [];
-			const arrP2 = [];
 			
 			for ( let i = 0; i < arrL.length; i++ )
 			{
@@ -168,10 +191,6 @@ class MyGeneratorWFZmyka
 					
 					if(onLine)
 					{				
-						const dist = pos.distanceTo(startPos);
-						
-						arrP2.push({dist, pos});
-
 						vL.push(pos);
 						
 						//this.crHelpBox({pos});
@@ -182,34 +201,38 @@ class MyGeneratorWFZmyka
 				// выстраиваем точки в одну линию
 				if(vL.length > 0)
 				{
-					if(vL.length > 2)
+					const arrP = [];
+					const bBox = this.getBox({v: vL});
+					const dir = v2.clone().sub(v1).normalize();
+					const posTest = myMath.multiVector3({dir, num: 6});
+					posTest.add(v1);
+					
+					for ( let i2 = 0; i2 < vL.length; i2++ )
 					{
-						const arrP = [];
-						const bBox = this.getBox({v: vL});
+						const dist = vL[i2].distanceTo(posTest);				
+						arrP.push({dist, pos: vL[i2]});
 						
-						for ( let i2 = 0; i2 < vL.length; i2++ )
-						{
-							const dist = vL[i2].distanceTo(bBox[0]);				
-							arrP.push({dist, pos: vL[i2]});
-
-							this.crHelpBox({pos: vL[i2]});
-						}
-						
-						arrP.sort((a, b) => { return a.dist - b.dist; });
-						
-						vL = [];
-						
-						for ( let i2 = 0; i2 < arrP.length; i2++ )
-						{
-							vL.push(arrP[i2].pos);
-						}
-						
+						//this.crHelpBox({pos: posTest, color: 0x00ff00, size: 0.5});
 					}
 					
+					//this.crHelpLine({v: vL, color: 0x00ff00});
+					
+					arrP.sort((a, b) => { return a.dist - b.dist; });
+					
+					vL = [];
+					
+					for ( let i2 = 0; i2 < arrP.length; i2++ )
+					{
+						vL.push(arrP[i2].pos);
+					}					
+						
 					arrL2.push({v: vL});
 				}
 			}
 
+			// проверяем каждую линию на то чтобы она была не разрывной (то есть, может быть что центральная часть находится за пределами сетки)
+			// например если сетка формой П, поэтому линию разделяем
+			// в линии может быть более 2-х точек, нужно получить только первую и последнюю
 			const arrL3 = [];
 			
 			for ( let i = 0; i < arrL2.length; i++ )
@@ -247,7 +270,11 @@ class MyGeneratorWFZmyka
 						else
 						{
 							if(crossL.length === 0) crossL.push([v1, v2]);
-							else crossL[crossL.length - 1].push(v1, v2);
+							else 
+							{
+								crossL[crossL.length - 1].push(v1, v2);
+								console.log(crossL[crossL.length - 1]);
+							}
 						}
 					}
 				}
@@ -258,6 +285,9 @@ class MyGeneratorWFZmyka
 					{
 						//console.log(crossL[i2]);
 						arrL3.push({v: [crossL[i2][0], crossL[i2][crossL[i2].length - 1]]});
+						
+						this.crHelpBox({pos: crossL[i2][0], color: 0xff0000});
+						this.crHelpBox({pos: crossL[i2][crossL[i2].length - 1], color: 0x0000ff});
 					}					
 				}				
 			}
@@ -290,42 +320,18 @@ class MyGeneratorWFZmyka
 						ind = result.ind;
 						n = result.n;						
 					}
+					if(!result) break;
 				}
 				
 				
 				this.crHelpLine({v: newLine});
+				
+				if(this.typeZmyka === 2) this.offsetContour({v: newLine, offset: sizeCell});	// обратка
 				//console.log(arrL3.length, newLine);				
 			}
 		}
 		
 		
-
-		
-		if(1===2)
-		{
-			this.crHelpBox({pos: arrP2[0].pos, color: 0xff0000, size: 0.1});
-			this.crHelpBox({pos: arrP2[arrP2.length - 1].pos, color: 0x00ff00, size: 0.1});			
-			
-			const arrP3 = [];
-			let n = 0;
-			for ( let i = 0; i < arrP2.length - 1; i+=2 )
-			{
-				// четное число 0, 2, 4 и т.д.
-				if(n % 2 === 0) 
-				{
-					arrP3.push(arrP2[i].pos, arrP2[i+1].pos);
-					if(i + 3 < arrP2.length) arrP3.push(arrP2[i+3].pos);
-				}
-				else
-				{
-					arrP3.push(arrP2[i+1].pos, arrP2[i].pos);
-				}
-				
-				n++;
-			}
-			
-			this.crHelpLine({v: arrP3});			
-		}
 	}
 
 
@@ -365,14 +371,20 @@ class MyGeneratorWFZmyka
 	}
 
 
+	// находим ближайшую линии к targetPos
+	// определяем к какой точки линии нужно присоединиться 
+	// делаем проверку, чтобы полученная точка не находилась за пределами	
 	jointLines({ind, arrL3, n, pGrid})
 	{
 		const vLine = arrL3[ind].v;
 		
-		arrL3.splice(ind, 1);
+		arrL3.splice(ind, 1);	// удаляем линию (к которой относится targetPos) из массива расчета 
 		
 		const ind1 = (n === 1) ? vLine.length - 1 : 0;
-		const pos1 = vLine[ind1];
+		const ind2 = (n === 1) ? 0 : vLine.length - 1;
+		const targetPos = vLine[ind1];
+		
+		const dir1 = targetPos.clone().sub(vLine[ind2]).normalize();
 		
 		const arrP1 = [];
 		
@@ -381,14 +393,27 @@ class MyGeneratorWFZmyka
 			const pos2 = arrL3[i].v[0];
 			const pos3 = arrL3[i].v[arrL3[i].v.length - 1];
 			
-			const dist1 = pos1.distanceTo(pos2);
-			const dist2 = pos1.distanceTo(pos3);
+			const dir2 = pos2.clone().sub(pos3).normalize();
+			const dir3 = pos3.clone().sub(pos2).normalize();
+			
+			const posProject = myMath.mathProjectPointOnLine2D({A: pos2, B: pos3, C: targetPos});
+			
+			const dist = targetPos.distanceTo(posProject);
+			const dist1 = posProject.distanceTo(pos2);
+			const dist2 = posProject.distanceTo(pos3);
 			
 			const n2 = (n !== 1) ? 1 : 2;
 			const n1 = (n !== 1) ? 2 : 1;
 			
-			arrP1.push({dist: dist1, ind: i, pos: pos2, n: n1});
-			arrP1.push({dist: dist2, ind: i, pos: pos3, n: n2});
+			//if(dist1 < dist2)
+			if(dir1.dot(dir2) > 0.9)
+			{
+				arrP1.push({dist, ind: i, pos: pos2, n: n1});								
+			}
+			else
+			{
+				arrP1.push({dist, ind: i, pos: pos3, n: n2});
+			}
 		}
 
 		let result = null;
@@ -403,11 +428,13 @@ class MyGeneratorWFZmyka
 				const pos = arrP1[i].pos;
 				const n = arrP1[i].n;
 				
-				const v1 = pos1;
+				const v1 = targetPos;
 				const v2 = pos;
 				
 				let cross = false;
 				
+				// определяем, если созданная линия из полученной точки и предидущей точки 
+				// пересекаются с контуром пола, то выкидываем эту точка и расчета и смотрим другие
 				for ( let i3 = 0; i3 < pGrid.length; i3++ )
 				{
 					const v3 = pGrid[i3];
@@ -425,14 +452,74 @@ class MyGeneratorWFZmyka
 				}
 				
 			}
-			
-			
+						
 			//this.crHelpBox({pos: pos, color: 0xff0000, size: 0.1});
-			
 			
 		}
 
 		return result;		
+	}
+	
+	
+	offsetContour({v, offset})
+	{
+		const newFormPoints = this.offsetForm({points: v, offset});
+		
+		console.log(111112, newFormPoints);
+		
+		this.crHelpLine({v: newFormPoints, color: 0x0000ff});
+	}
+	
+
+	// смещение формы (точки должны идти против часавой) (контру должен быть замкнут, последняя точка ровна первой)
+	offsetForm({points, offset}) 
+	{
+		const lines = this.offsetLines({points, offset});
+
+		const pt1 = myMath.intersectionTwoLines_1({line1: lines[0], line2: lines [lines.length - 1]});
+		const pointsOffset = [ new THREE.Vector3( pt1.x, 0, pt1.z ) ];
+
+		for ( let i = 0; i < lines.length - 1; i++ ) 
+		{
+			const pt = myMath.intersectionTwoLines_1({line1: lines[i], line2: lines [i + 1]});
+
+			pointsOffset.push( new THREE.Vector3( pt.x, 0, pt.z ) );
+		}
+
+		const closed = (points[0].distanceTo(points[points.length - 1]) > 0.0001) ? false : true;	// закнут контру или нет
+		if(closed) pointsOffset.push( pointsOffset[0].clone() );
+
+		return pointsOffset;
+	}
+	
+	
+	// смещение массива точек контура, отдаем массив линий (контру должен быть замкнут, последняя точка ровна первой)
+	offsetLines({points, offset})
+	{
+		const lines = [];
+		
+		// проверяем закунт ли контур, если нет, то делаем замкнутый массив точек
+		if(points[0].distanceTo(points[points.length - 1]) > 0.0001)	
+		{
+			points = [...points]; 	// копируем массив, создав новый
+			points.push(points[0]);
+		}
+		
+		for ( let i = 0; i < points.length - 1; i++ ) 
+		{
+			let pt1 = points[ i ];
+			let pt2 = points[ i + 1 ];
+
+			const dir = pt2.clone().sub( pt1 );
+			const angle = Math.atan2( dir.z, dir.x );
+
+			const offsetPt1 = new THREE.Vector3( pt1.x - offset * Math.cos( angle - Math.PI / 2 ), 0, pt1.z + offset * Math.sin( angle + Math.PI / 2 ) );
+			const offsetPt2 = new THREE.Vector3( pt2.x - offset * Math.cos( angle - Math.PI / 2 ), 0, pt2.z + offset * Math.sin( angle + Math.PI / 2 ) );
+
+			lines.push({start: offsetPt1, end: offsetPt2});			
+		}
+
+		return lines;
 	}
 	
 
